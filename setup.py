d4e21c400799dc8433a1acda04979b8f48e9a7b9
contrust <2001raf@gmail.com>
2021-12-05 12:08:23 +0500

    portscan command in console after setup

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='portscaner',
     version='1.0.0',
-    packages=find_packages(include=['portscaner', 'portscaner.*'])
+    packages=find_packages(include=['portscaner', 'portscaner.*']),
+    entry_points={
+        'console_scripts': [
+            'portscan = portscaner.__main__:main',
+        ]
+    }
 )
